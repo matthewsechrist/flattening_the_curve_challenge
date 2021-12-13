7576472badcd0eac13d6eaf7cf80d9349f0bfd35
MattSechrist <matthew.sechrist@gmail.com>
2021-12-13 14:41:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ lambda_client = boto3.client('lambda')
 
 
 for document_number in range (1,101):
+    payload = json.dumps({"document_number":document_number})
+
     response = lambda_client.invoke(
         FunctionName='trigger',
         InvocationType='Event',
-        Payload=json.dumps({"document_number":document_number}),
+        Payload=payload,
     )
+    print(document_number)
+    print(payload)
 
 print(response['Payload'])
 print(response['Payload'].read().decode("utf-8"))
